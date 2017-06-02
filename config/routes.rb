506e41882ee34a77bb684f076ce18bf2f4bbbfd7
superbuggy <jpqreichard@gmail.com>
2017-06-02 13:09:02 -0400

    fixes missing path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get    '/artists'           => 'artists#index'
-  get    '/artists/new'       => 'artists#new'
-  get    '/artists/:id'       => 'artists#show'
+  get    '/artists/:id'       => 'artists#show', as: "artist"
+  get    '/artists/new'       => 'artists#new', as: "new_artist"
   post   '/artists'           => 'artists#create'
-  get    '/artists/:id/edit'  => 'artists#edit'
+  get    '/artists/:id/edit'  => 'artists#edit', as: "edit_artist"
   put    '/artists/:id'       => 'artists#update'
   delete '/artists/:id'       => 'artists#destroy'
 end
