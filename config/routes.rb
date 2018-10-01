20dd515cc9c1ecabbf3e628dbe446bc01137fd21
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-10-01 17:24:33 -0400

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get    '/artists'           => 'artists#index'
-  get    '/artists/:id'       => 'artists#show', as: "artist"
   get    '/artists/new'       => 'artists#new', as: "new_artist"
+  get    '/artists/:id'       => 'artists#show', as: "artist"
   post   '/artists'           => 'artists#create'
   get    '/artists/:id/edit'  => 'artists#edit', as: "edit_artist"
   put    '/artists/:id'       => 'artists#update'
